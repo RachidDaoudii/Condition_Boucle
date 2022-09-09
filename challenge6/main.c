09575605dae2fdf532777ceb8a5fbe42f697eab4
rachiddaoudi <rachiddaoudi533@gmail.com>
2022-09-09 12:02:24 +0100

    add

--- challenge6/main.c
+++ challenge6/main.c
@@ -4,18 +4,20 @@
 
 int main()
 {
-    char mot1[100],mot2[100];
-    int res;
+    char mot1[4],mot2[4];
+    int res,i;
     printf("Enter le premier mot :");
     scanf("%s",&mot1);
     printf("Enter la deuxeme mot :");
     scanf("%s",&mot2);
-    res =strncmp(mot1,mot2,100);
-    if (res<0 || res==0)
+    /*res =strncmp(mot1,mot2,4);
+    if (res==0 || res >0)
         printf("les mots ont les memes lettres");
     else
-        printf("les mots non les memes lettres");
+        printf("les mots non les memes lettres");*/
+        for(i=0;i<4;i++){
 
+        }
 
     return 0;
 }
